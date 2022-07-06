bf190f915df61821dbd59c196499eec50f43d7e6
Jordan Delgado <jordelquino@gmail.com>
2022-07-05 20:49:11 -0500

    Fixing issues

---
+++ assets/js/words.js
@@ -0,0 +1,16 @@
+// Change text on interval
+// array to store strings
+var words = [
+    "research.",
+    "UX.",
+    "UI.",
+    "design.",
+    "personas.",
+];
+
+var i = 1 // Start at 1 because 0 is already displayed
+
+var animInterval = window.setInterval(() => {
+    document.querySelector(".interval").textContent = words[i];
+    i = ++i % words.length; // Add 1 until it reaches 5, then back to 0, 1, 2...
+}, 1000);
