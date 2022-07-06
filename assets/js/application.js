d2a8857489bd9a3fd7a2d70dc0188b97dc78d1ac
Jordan Delgado <jordelquino@gmail.com>
2022-07-05 20:14:17 -0500

    Dele some code lines

--- assets/js/application.js
+++ assets/js/application.js
@@ -72,16 +72,19 @@ var animInterval = window.setInterval(() => {
     i = ++i % words.length; // Add 1 until it reaches 5, then back to 0, 1, 2...
 }, 1000);
 
-if (window.netlifyIdentity) {
-    window
-    .netlifyIdentity
-    .on("init", user => {
-        if (!user) {
-        window
-            .netlifyIdentity
-            .on("login", () => {
-            document.location.href = "/admin/#";
-            });
-        }
-    });
-}
+
+
+
+// if (window.netlifyIdentity) {
+//     window
+//     .netlifyIdentity
+//     .on("init", user => {
+//         if (!user) {
+//         window
+//             .netlifyIdentity
+//             .on("login", () => {
+//             document.location.href = "/admin/#";
+//             });
+//         }
+//     });
+// }
